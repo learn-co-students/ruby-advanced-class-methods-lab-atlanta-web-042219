ee46bf5590b8f58c8945d300629202ba2cecfcfa
Andrew Allen <asallen67@gmail.com>
2019-04-24 23:01:42 -0400

    optimized and cleaned

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,63 @@
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
+
+#--------------------------------------------------#
 
   def self.all
     @@all
   end
 
+  def self.destroy_all
+  	@@all.clear
+  end
+
   def self.create
   	new_song = self.new
-  	@@all << new_song
+  	new_song.save
+  end
+
+  def save
+    Song.all << self
+    self
+  end
+
+#--------------------------------------------------#
+
+  def self.by_name(obj_name)
+  	new_song = self.new
+  	new_song.name = obj_name
   	new_song
   end
 
   def self.new_by_name(obj_name)
-  	new_song = self.new
-  	new_song.name = obj_name
-  	new_song
+  	self.by_name(obj_name)
   end
 
   def self.create_by_name(obj_name)
-  	new_song = self.new
-  	new_song.name = obj_name
-  	new_song.save
+  	self.by_name(obj_name).save
+  end
+
+#--------------------------------------------------#
+
+  def self.from_file(file)
+  	new_song = Song.new
+  	file = file.split(/\ - |\./)
+  	new_song.artist_name = file[0]
+  	new_song.name = file[1]
   	new_song
   end
+
+  def self.new_from_filename(file)
+  	self.from_file(file)
+  end
+
+  def self.create_from_filename(file)
+  	self.from_file(file).save
+  end
+
+#--------------------------------------------------#
 
   def self.find_by_name(search)
   	@@all.each {|obj|return obj if obj.name == search}
@@ -43,29 +77,5 @@ class Song
   	@@all.sort_by {|obj| obj.name}
   end
 
-  def self.new_from_filename(file)
-  	new_song = Song.new
-  	file = file.split(/\ - |\./)
-  	new_song.artist_name = file[0]
-  	new_song.name = file[1]
-  	new_song
-  end
-
-  def self.create_from_filename(file)
-  	new_song = Song.new
-  	file = file.split(/\ - |\./)
-  	new_song.artist_name = file[0]
-  	new_song.name = file[1]
-  	new_song.save
-  	new_song
-  end
-
-  def self.destroy_all
-  	@@all.clear
-  end
-
-  def save
-    Song.all << self
-  end
-
+#--------------------------------------------------#
 end
